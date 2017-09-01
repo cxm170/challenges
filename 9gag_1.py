534e734bf00ae6972988a16e18e27a84c3081817
Jiwei Li <cxm170@gmail.com>
2017-09-01 10:48:32 +0800

    add comments

--- 9gag_1.py
+++ 9gag_1.py
@@ -1,12 +1,16 @@
 import pandas as pd
-pd.set_option('display.max_columns', None)
 
+# Assume the raw data file is stored in the same directory
 df = pd.read_csv('./reddit_posts_2016_09_week_1.csv')
 
+# Format the timestamp to date
 df['created_utc'] = pd.to_datetime(df['created_utc'], unit='s', utc=True).apply(lambda x: x.date())
 
+# Get the leaderboards for different subreddits in different days
 df_agg = df.groupby(['created_utc', 'subreddit','author'], as_index=False).title.count().sort_values(['created_utc', 'subreddit', 'title'], ascending=[True, True, False])
 
+# Rename the 'author' column to 'count'
 df_agg.columns.values[3] = 'count'
 
+# Save to a local csv file
 df_agg.to_csv('9gag_1.csv', sep='\t', index=False)
--- 9gag_2.py
+++ 9gag_2.py
@@ -1,5 +1,11 @@
+# Basic idea:
+# 1. Get a list with distinct tuples (created_utc, author)
+# 2. Convert to created_utc to special code
+# 3. Sum up those created_utc codes for each author
+# 4. Find the longest consecutive '1's in the sum (converted to String) of each author
+# 5. Sort by the streak
+
 import pandas as pd
-import numpy as np
 
 # Encode day to one of 1, 10, 100, 1000, ...
 def encodeDay(x):
@@ -9,7 +15,7 @@ def encodeDay(x):
         result += '0'
     return int(result)
 
-# Given a string, find the longest substring with repetitive '1'
+# Given a string, find the longest substring with repetitive '1's
 def getStreak(x):
     x = str(x)
     streak = 0
@@ -33,19 +39,22 @@ def getStreak(x):
         streak = tmpStreak
     return streak
 
-
-pd.set_option('display.max_columns', None)
-
 df = pd.read_csv('./reddit_posts_2016_09_week_1.csv')
 
+# Convert created_utc to day (1, 2, ..., 31)
 df['created_utc'] = pd.to_datetime(df['created_utc'], unit='s', utc=True).apply(lambda x: x.day)
 
+# Get a list with distinct tuples (created_utc, author)
 g = df.groupby(['created_utc', 'author'], as_index=False).subreddit.count()
 
+# Convert to created_utc to special code
 g['code'] = g['created_utc'].apply(lambda x:encodeDay(x))
 
+# Sum up those created_utc codes for each author
 result = g.groupby(['author'], as_index=False).code.sum()
 
+# Find the longest consecutive '1's in the sum (converted to String) of each author
 result['streak'] = result['code'].apply(lambda x: getStreak(x))
 
+# Sort by streak, and save to a local file
 result.sort_values(['streak', 'author'], ascending=[False, True]).to_csv('9gag_2.csv', sep='\t', index=False)
